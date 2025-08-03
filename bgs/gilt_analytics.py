7fd866394e2ec0c048d3e1fa47409d722388f21a
Cormac Hollingsworth <16384725+cormach@users.noreply.github.com>
2025-08-03 13:20:24 +0000

    feat: ql calcs

--- bgs/gilt_analytics.py
+++ bgs/gilt_analytics.py
@@ -1,22 +1,27 @@
 import QuantLib as ql
 
-settlementDays=2
-calendar=ql.TARGET()
-faceAmount=100.0
-startDate=ql.Date(15,12,2019)
-maturityDate=ql.Date(15,12,2024)
-tenor=ql.Period('1Y')
-coupon=[0.05]
-paymentConvention=ql.ActualActual(ql.ActualActual.Bond)
-schedule = ql.MakeSchedule(startDate, maturityDate, tenor)
+schedule = ql.Schedule(
+    ql.Date(15, 12, 2019),
+    ql.Date(15, 12, 2029),
+    ql.Period('1Y'),
+    ql.TARGET(),
+    ql.Unadjusted,
+    ql.Unadjusted,
+    ql.DateGeneration.Backward,
+    False
+)
 
 bond = ql.FixedRateBond(
-    settlementDays=settlementDays,
-    faceAmount=faceAmount,
+    settlementDays=2,
+    faceAmount=100.0,
     schedule=schedule,
-    coupons=coupon,
-    paymentConvention=paymentConvention,
-    issueDate=startDate,
+    coupons=[0.05],
     paymentDayCounter=ql.ActualActual(ql.ActualActual.Bond),
-    maturityDate=maturityDate,
-    )
+)
+p= bond.cleanPrice(
+    0.03,
+    ql.ActualActual(ql.ActualActual.Bond),
+    ql.Compounded,
+    ql.Annual,
+)
+print(f"Clean Price: {p:.2f}")
