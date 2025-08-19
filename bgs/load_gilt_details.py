fa198992cfb7c427c219b11f52bb54322c3935d3
Cormac Hollingsworth <16384725+cormach@users.noreply.github.com>
2025-08-19 15:53:14 +0000

    feat: boe portfolio analysis

--- bgs/load_gilt_details.py
+++ bgs/load_gilt_details.py
@@ -93,7 +93,8 @@ def load_csv_blocks(file_path, encoding="latin1"):
                 case "END":
                     collecting_data = False
                     break
-    
+    # incredibly the BGS has a data error on the id in the details
+    dataframes["Conventionals"]=dataframes["Conventionals"].replace('32112', '32120')
     try:
         assert (dataframes["Conventionals"]['Sequence'].astype(float) < 50000).all()
         assert (dataframes["Index-Linked New-style"]['Sequence'].astype(float) < 60000).all()
